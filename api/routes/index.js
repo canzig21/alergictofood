b0f70562865a91b77c3d2c3406dba47eb52ab133
sbarnabas <sbarnabas@gmail.com>
2018-11-10 12:57:39 -0500

    getItem placeholder

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getItem',function(req,res,next) {
+  res.json({
+    "id":"aaa",
+    "name":"Almonds",
+    "attributes": ["nuts","organic","raw"],
+    "url":"www.almonds.com",
+    "ingredients":["aab","aac"],
+    "history":["bbb","ccc","ddd"]
+  
+  })
+})
+
 module.exports = router;
